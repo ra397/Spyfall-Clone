a0cc58ff49dd0ccf77f070ef34e7c2ce56d5a70f
Alaya <ralaya@uiowa.edu>
2025-06-25 15:31:45 -0500

    Fixed host address

--- app.py
+++ app.py
@@ -103,4 +103,4 @@ def handle_end_round(data):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='127.0.0.1', port=port)
+    socketio.run(app, host='0.0.0.0', port=port)
